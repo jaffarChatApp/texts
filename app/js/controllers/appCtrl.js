4c7055dc44de275d4d8c58ec82ac535bc7f2e74a
Jaffar <37204341+jaffarChatApp@users.noreply.github.com>
2018-09-01 15:19:36 +0530

    Callback testing

--- app/js/controllers/appCtrl.js
+++ app/js/controllers/appCtrl.js
@@ -30,7 +30,8 @@ ChatApp.controller('appCtrl', ['$scope', '$rootScope', '$http', '$state', '$sess
     $scope.getUsers = function () {
         $http({
             method: 'GET',
-            url: 'https://github.com/jaffarChatApp/phpGitServer/blob/master/index.php?function=testing',
+            //url: 'https://github.com/jaffarChatApp/phpGitServer/blob/master/index.php?function=testing',
+            url: 'https://gitlab.com/jaffargit/chatApp/blob/master/Server/user.php',
             //url: CONFIG.rootUrl + 'function=getUserList',
             headers: {
                 'Content-Type': 'application/json'
