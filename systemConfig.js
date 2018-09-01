6d2accbd78e9ff2b57c693f96e583ad5c557ff8d
Jaffar <37204341+jaffarChatApp@users.noreply.github.com>
2018-09-01 11:53:28 +0530

    Url testing

--- systemConfig.js
+++ systemConfig.js
@@ -1,8 +1,19 @@
 /*Git URL Configuration*/
-ChatApp.constant('CONFIG', {
+/*ChatApp.constant('CONFIG', {
     'name': 'Chat App',
     'version': '1.0',
     'rootUrl': "https://jaffarchatapp.github.io/texts/serverApi/index.php?", // Local Server
+    'chatUrl': "ws://localhost:1880/ws/getChats", // Local WS
+    'notifyUrl': "https://onesignal.com/api/v1/notifications", // Notification URL
+    'REST_API_KEY': "Basic ZWJjOWQxNDEtYTg5ZS00YmY2LWI5YmQtMmZmMmIwNTBlZDM1", // REST API KEY
+    'app_id': "fe9010f6-0bb3-464a-9e55-6aab21b2f944", // App Id
+});*/
+
+ChatApp.constant('CONFIG', {
+    'name': 'Chat App',
+    'version': '1.0',
+    'rootUrl': "https://github.com/jonls/php-git-server/blob/master/index.php?", // Local Server
+    //'rootUrl': "https://jaffarchatapp.github.io/texts/serverApi/index.php?", // Local Server
     'chatUrl': "ws://localhost:1880/ws/getChats", // Local WS
     'notifyUrl': "https://onesignal.com/api/v1/notifications", // Notification URL
     'REST_API_KEY': "Basic ZWJjOWQxNDEtYTg5ZS00YmY2LWI5YmQtMmZmMmIwNTBlZDM1", // REST API KEY
